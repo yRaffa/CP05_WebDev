60e5d9417842dcdb946ebbe0f50973f086e6937f
Rafael Oliveira <yraffa.olive@gmail.com>
2024-10-09 13:01:09 -0300

    Creating ReactApp

---
+++ gourmet-on/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Apresentacao from './Components/Apresentacao.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Apresentacao />
+    </div>
+  );
+}
+
+export default App;
---
+++ gourmet-on/src/Components/Apresentacao.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Apresentacao = () => {
+    return (
+        <div></div>
+    )
+};
+
+export default Apresentacao;
