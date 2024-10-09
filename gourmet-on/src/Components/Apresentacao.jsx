dda647736cffd6765328f29db1d62467be40a09a
Rafael Oliveira <yraffa.olive@gmail.com>
2024-10-09 14:42:46 -0300

    Update

--- gourmet-on/src/Components/Apresentacao.jsx
+++ gourmet-on/src/Components/Apresentacao.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
-import ApresentacaoBG from '../Assets/bg-apresentacao.png';
-import ImgApresentacaoBG from '../Assets/bg-apresentacao-imagem.png';
+import BGLeft from '../Assets/bg-left.png';
+import BGRight from '../Assets/bg-right.png';
+import ApresentacaoImagem from '../Assets/apresentacao-imagem.png';
 import {BsFillPlayCircleFill} from 'react-icons/bs';
-
 
 const Apresentacao = () => {
     return (
         <div className='about-section-container'>
-            <div className='about-backgroud-image-container'>
-                <img src={ApresentacaoBG} alt="" />
+            <div className='about-background-image-container-left'>
+                <img src={BGLeft} />
             </div>
             <div className='about-section-image-container'>
-                <img src={ImgApresentacaoBG} alt="" />
+                <img src={ApresentacaoImagem} />
             </div>
             <div className='about-section-text-container'>
-                <p className='primary-subheading'>Apresentação</p>
-                <h1 className='primary=heading'>Lorem ipsum dolor sit amet.</h1>
-                <p className='primary-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, necessitatibus?</p>
-                <p className='primary-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente optio unde eos?</p>
+                <p className='primary-subheading'>SOBRE NÓS</p>
+                <h1 className='primary=heading'>BENEFÍCIOS</h1>
+                <li className='primary-text'>Os melhores e mais eficientes entregadores.</li>
+                <li className='primary-text'>Ampla variedade de tipos de restaurante.</li>
+                <li className='primary-text'>Sistema de pagamentos moderno, rápido e fácil.</li>
                 <div className='about-buttons-container'>
                     <button className='secondary-button'>Saiba Mais</button>
-                    <button className='watch-video-button'> <BsFillPlayCircleFill/> Assista ao Video</button>
+                    <button className='watch-video-button'> <BsFillPlayCircleFill/> Video de Apresentação</button>
                 </div>
+            </div>
+            <div className='about-background-image-container-right'>
+                <img src={BGRight} />
             </div>
         </div>
     )
